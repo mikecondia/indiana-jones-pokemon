123d1e6a57d7e9fd47c198abb4cb66bb31e7b7eb
Mike Condia <mikecondia@gmail.com>
2024-12-28 09:51:24 -0800

    add bulbaasar gif

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-axios.get('https://pokeapi.co/api/v2/pokemon?limit=151').then((res)=>{
-    console.log(res.data)
-}); 
+
+
+// const pokemonImage = document.getElementById("pokemon-image"); 
+
+
+
+// axios.get('https://pokeapi.co/api/v2/pokemon?limit=151').then((res)=>{
+//     console.log(res.data)
+// }); 
+// axios.get('sprites/pokemon/versions/generation-v/black-white/animated/100.gif').then((res)=>{
+//     console.log(res.data)
+// })
