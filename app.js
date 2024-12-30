157e579860b2b445d315ec83ea72b69feab2fced
Mike Condia <mikecondia@gmail.com>
2024-12-30 11:26:11 -0800

    create pokemon array

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const imageUrls = ["images/1.gif", "images/4.gif", "images/7.gif", "images/10.gif","images/12.gif",
+    "images/13.gif", "images/16.gif", "images/19.gif", "images/21.gif", "images/23.gif", "images/25.gif","images/27.gif",
+    "images/29.gif","images/32.gif","images/35.gif","images/37.gif","images/39.gif","images/41.gif", "images/43.gif","images/46.gif","images/48.gif","images/50.gif",
+    "images/52.gif", "images/54.gif","images/56.gif", "images/58.gif", "images/60.gif", 
+];
+const container = document.getElementById("imageContainer"); 
+
+
+
+
+
+imageUrls.forEach(url => {
+  const img = new Image();
+  img.src = url;
+  img.alt = "Image"; // Set alt text for accessibility
+  container.appendChild(img);
+});
 
 
 
@@ -6,15 +23,25 @@
 
 
 
-// const pokemonContainer = document.getElementById("pokemon-container"); 
 
-// // Simulating getting a list of image files from a folder
-// const imageFiles = ["1.gif", "4.gif", "7.gif"]; 
+// const imageUrls = [
+//     "images/1.gif"
+// ]; 
 
-// for (const imageFile of imageFiles) {
-//   const image = new Image(); 
-//   image.src = `images/${imageFile}`; 
-//   image.alt = "Image from folder"; 
+// function getRandomImageUrl() {
+//     const randomIndex = Math.floor(Math.random() * imageUrls.length);
+//     return imageUrls[randomIndex];
+//   }
 
-//   imageContainer.appendChild(image); 
-// }
+//   function appendRandomImage() {
+//     const imageUrl = getRandomImageUrl();
+//     const imageElement = document.createElement("img");
+//     imageElement.src = imageUrl;
+//     document.body.appendChild(imageElement);
+//   }
+
+
+
+
+
+
