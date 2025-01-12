4e0120906fe0d18abfbe47d702724b8152b6f8ae
Mike Condia <mikecondia@gmail.com>
2025-01-11 19:18:31 -0800

    finish append

--- app.js
+++ app.js
@@ -7,50 +7,50 @@ const pokemonArray = ["images/1.gif", "images/4.gif", "images/7.gif", "images/10
      "images/102.gif", "images/104.gif","images/108.gif", "images/109.gif", "images/113.gif", "images/114.gif",
     "images/116.gif","images/118.gif","images/120.gif","images/122.gif","images/129.gif", "images/132.gif","images/133.gif","images/138.gif","images/147.gif","images/151.gif","images/175.gif"
 ];
-const slot1 = document.getElementById("image-container-1");
-const slot2 = document.getElementById("image-container-2");
-const slot3 = document.getElementById("image-container-3");
-const slot4 = document.getElementById("image-container-4");
-const slot5 = document.getElementById("image-container-5");
-const slot6 = document.getElementById("image-container-6");
-const slot7 = document.getElementById("image-container-7"); 
-const slot8 = document.getElementById("image-container-8");
-const slot9 = document.getElementById("image-container-9");
-const slot10 = document.getElementById("image-container-10");
-const slot11 = document.getElementById("image-container-11");
-const slot12 = document.getElementById("image-container-12");
-const slot13 = document.getElementById("image-container-13");
-const slot14 = document.getElementById("image-container-14"); 
-const slot15 = document.getElementById("image-container-15");
-const slot16 = document.getElementById("image-container-16");
-const slot17 = document.getElementById("image-container-17");
-const slot18 = document.getElementById("image-container-18");
-const slot19 = document.getElementById("image-container-19");
-const slot20 = document.getElementById("image-container-20");
-const slot21 = document.getElementById("image-container-21"); 
-const slot22 = document.getElementById("image-container-22");
-const slot23 = document.getElementById("image-container-23");
-const slot24 = document.getElementById("image-container-24");
-const slot25 = document.getElementById("image-container-25");
-const slot26 = document.getElementById("image-container-26");
-const slot27 = document.getElementById("image-container-27");
-const slot28 = document.getElementById("image-container-28"); 
-const slot29 = document.getElementById("image-container-29");
-const slot30 = document.getElementById("image-container-30");
-const slot31 = document.getElementById("image-container-31");
-const slot32 = document.getElementById("image-container-32");
-const slot33 = document.getElementById("image-container-33");
-const slot34 = document.getElementById("image-container-34");
-const slot35 = document.getElementById("image-container-35"); 
-const slot36 = document.getElementById("image-container-36");
-const slot37 = document.getElementById("image-container-37");
-const slot38 = document.getElementById("image-container-38");
-const slot39 = document.getElementById("image-container-39");
-const slot40 = document.getElementById("image-container-40"); 
+// const slot1 = document.getElementById("image-container-1");
+// const slot2 = document.getElementById("image-container-2");
+// const slot3 = document.getElementById("image-container-3");
+// const slot4 = document.getElementById("image-container-4");
+// const slot5 = document.getElementById("image-container-5");
+// const slot6 = document.getElementById("image-container-6");
+// const slot7 = document.getElementById("image-container-7"); 
+// const slot8 = document.getElementById("image-container-8");
+// const slot9 = document.getElementById("image-container-9");
+// const slot10 = document.getElementById("image-container-10");
+// const slot11 = document.getElementById("image-container-11");
+// const slot12 = document.getElementById("image-container-12");
+// const slot13 = document.getElementById("image-container-13");
+// const slot14 = document.getElementById("image-container-14"); 
+// const slot15 = document.getElementById("image-container-15");
+// const slot16 = document.getElementById("image-container-16");
+// const slot17 = document.getElementById("image-container-17");
+// const slot18 = document.getElementById("image-container-18");
+// const slot19 = document.getElementById("image-container-19");
+// const slot20 = document.getElementById("image-container-20");
+// const slot21 = document.getElementById("image-container-21"); 
+// const slot22 = document.getElementById("image-container-22");
+// const slot23 = document.getElementById("image-container-23");
+// const slot24 = document.getElementById("image-container-24");
+// const slot25 = document.getElementById("image-container-25");
+// const slot26 = document.getElementById("image-container-26");
+// const slot27 = document.getElementById("image-container-27");
+// const slot28 = document.getElementById("image-container-28"); 
+// const slot29 = document.getElementById("image-container-29");
+// const slot30 = document.getElementById("image-container-30");
+// const slot31 = document.getElementById("image-container-31");
+// const slot32 = document.getElementById("image-container-32");
+// const slot33 = document.getElementById("image-container-33");
+// const slot34 = document.getElementById("image-container-34");
+// const slot35 = document.getElementById("image-container-35"); 
+// const slot36 = document.getElementById("image-container-36");
+// const slot37 = document.getElementById("image-container-37");
+// const slot38 = document.getElementById("image-container-38");
+// const slot39 = document.getElementById("image-container-39");
+// const slot40 = document.getElementById("image-container-40"); 
 
-locationsArray = [slot1,slot2,slot3,slot4,slot5,slot6,slot7,slot8,slot9,slot10,slot11,slot12,slot13,slot14,slot15,slot16,
-  slot17,slot18,slot19,slot20,slot21,slot22,slot23,slot24,slot25,slot26,slot27,slot28,slot29,slot30,slot31,slot32,slot33,slot34,slot35,
-  slot36, slot37,slot38,slot39,slot40]; 
+// locationsArray = [slot1,slot2,slot3,slot4,slot5,slot6,slot7,slot8,slot9,slot10,slot11,slot12,slot13,slot14,slot15,slot16,
+//   slot17,slot18,slot19,slot20,slot21,slot22,slot23,slot24,slot25,slot26,slot27,slot28,slot29,slot30,slot31,slot32,slot33,slot34,slot35,
+//   slot36, slot37,slot38,slot39,slot40]; 
 
 // Shuffle the array (using the Fisher-Yates algorithm)
 function shuffle(array) {
@@ -66,32 +66,12 @@ const shuffledUrls = shuffle(pokemonArray);
 function appendImage() {
   if (shuffledUrls.length > 0) {
     // const container = document.getElementById("image-container-1");
-    // const container = document.querySelectorAll('.container-style');
+    const container = document.querySelectorAll('.container-style');
     const imageUrl = shuffledUrls.pop();
     const img = document.createElement('img');
-    img.src = imageUrl;
-    // container.appendChild(img);
-    // let locations = [];
-
-    // function addLocation(newLocation) {
-    //   if (!locations.includes(newLocation)) {
-    //     locations.push(newLocation);
-    
-    //     // Append the location to the DOM
-    //     let locationElement = document.createElement('div');
-    //     locationElement.textContent = newLocation;
-    //     document.getElementById('locationList').appendChild(locationElement);
-    //   }
-    // 
-    
-    const randomIndex = Math.floor(Math.random() * locationsArray.length);
-    locationsArray[randomIndex].appendChild(img);
-   
-
-function removeDuplicates(arr) {
-    return [...new Set(arr)];
-}
-  
+    img.src = imageUrl;   
+    const randomIndex = Math.floor(Math.random() * container.length);
+    container[randomIndex].appendChild(img);
   }
 }
 function randomInterval(callback, minDelay, maxDelay) {
@@ -105,7 +85,7 @@ function randomInterval(callback, minDelay, maxDelay) {
   execute(); // Start the first execution
 }
 randomInterval(() => {
-  }, 100, 200); // Random delay between 1-5 seconds
+  }, 100, 4000); // Random delay between 1-5 seconds
 
 
 
